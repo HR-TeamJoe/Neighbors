63c634b8b97d3bafceec2e550eb04be084bbaced
Jenny Cheng <jenny.e.cheng@gmail.com>
2017-07-01 14:17:23 -0400

    finish styling chatbox

--- app/Profile/PublicProfile/Messages.jsx
+++ app/Profile/PublicProfile/Messages.jsx
@@ -93,22 +93,24 @@ class Messages extends React.Component{
 
   render(){
     return (
-      <div className='messageBox'>
-        
-        
-          <ul className="msgUl">
+      <div className='messagePopup'>
+      <div className='col-xs-12 messageBox'>
+              
+          <ul className="msgList">
             {this.state.messages.map((eachMessage, idx) => 
               <MessageEntry key={idx} eachMessage={eachMessage} self={this.props.self} friend={this.props.friend} selfName={this.props.allProps.profile.firstName} friendName={this.props.friendName}/>
             )}
           </ul>
-          
-          <form className="row msgContainer" onSubmit={(e) => this.sendMessage(e)}>
-            <div className="col-xs-8">
-              <input className="msgInput" type="text" onChange={this.setMessage} value={this.state.message}/>
-            </div>
-            <button className="col-xs-4 msgSubmit" type="submit">Send</button>
-          </form>
         
+      </div>
+      <div className='col-xs-12 msgBar'>
+        <form className='row sendMessageForm' onSubmit={(e) => this.sendMessage(e)}>
+          <div className="col-xs-8 msgInputBar">
+            <input className="msgInput" type="text" onChange={this.setMessage} value={this.state.message}/>
+          </div>
+          <button className="col-xs-3 btn msgSubmit" type="submit">Send</button>
+        </form>
+      </div>
       </div>
     )
   }
